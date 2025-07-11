07f6ab50f8b8449e2ec60299edfc5d04de1659b2
Thevanshgupta <vansh3813.1@gmail.com>
2025-07-11 11:24:34 +0530

    Reverse Doubly linklist

---
+++ Linklist/reverse_doubly_linklist.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespaces std;
+
+class Node:
+    public:
+        int data;
+        Node* next;
+        Node* prev;
+
+        void Reverse(node* head, next,prev)
+
+int main() {
+
+
+    return 0;
+}
